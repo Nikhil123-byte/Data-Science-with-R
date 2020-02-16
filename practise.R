49c339654a5de2301486d46cf01004db11fd17be
Nikhil Sinha <61113262+Nikhil123-byte@users.noreply.github.com>
2020-02-16 18:45:18 +0530

    Add files via upload
    R Code

--- practise.R
+++ practise.R
@@ -1,7 +1,12 @@
-1+1
-2+3*4
-3^2
-exp(1)
-sqrt(10)
-pi
-2*pi*6378
+datasets::AirPassengers
+datasets::airquality
+data("airquality")
+
+airquality <- datasets :: airquality
+
+head(airquality)
+tail(airquality)
+airquality[,c(1,2)]
+airquality$Wind
+summary(airquality$Temp)
+summary(airquality)
